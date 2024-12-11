d4121f4494f2a25ab5a6633ee38e7e48ee78500d
Daniele <danielemaria1407@gmail.com>
2024-12-10 21:07:35 -0300

    mudanças

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,6 @@
 // firebase.js
 import { initializeApp } from 'firebase/app'; // Para inicializar o Firebase
-import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth'; // Para autenticação
+import { getAuth, GoogleAuthProvider } from 'firebase/auth'; // Importa auth e GoogleAuthProvider
 
 const firebaseConfig = {
   apiKey: "AIzaSyCfa--ZgB_JP583AfA1Gy4VUUqw3XaPVG0",
@@ -16,28 +16,10 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 // Obtém o serviço de autenticação
-const auth = getAuth(app);
+export const auth = getAuth(app);
 
-export { auth, createUserWithEmailAndPassword };
+// Exporta o GoogleAuthProvider para login com Google
+export const provider = new GoogleAuthProvider();
 
-import { initializeApp } from 'firebase/app'; // Para inicializar o Firebase
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyCfa--ZgB_JP583AfA1Gy4VUUqw3XaPVG0",
-  authDomain: "explora-matasul.firebaseapp.com",
-  projectId: "explora-matasul",
-  storageBucket: "explora-matasul.firebasestorage.app",
-  messagingSenderId: "17736031538",
-  appId: "1:17736031538:web:8b06f532c7add1d672cb2b",
-  measurementId: "G-ZC8NNTR5N5"
-};
-
-// Inicializa o Firebase
-const app = initializeApp(firebaseConfig);
-
-// Obtém o serviço de autenticação
-const auth = getAuth(app);
-const provider = new GoogleAuthProvider(); // Crie uma instância do GoogleAuthProvider
-
-export { auth, provider }; // Certifique-se de exportar ambos
+// Exporta a instância do Firebase caso precise em algum lugar
+export default app;
