3862e478e8c214e1af284508d060f135060679e5
Évelly Silva <silvasevelly@gmail.com>
2025-01-31 17:42:01 -0300

    Atualização no DAO Generico

--- src/services/FirestoreDAO.js
+++ src/services/FirestoreDAO.js
@@ -1,37 +1,86 @@
 import { db } from "@/firebaseConfig";
-import { addDoc, collection, deleteDoc, doc, getDoc, getDocs, updateDoc } from "firebase/firestore";
+import { collection, doc, getDocs, query, setDoc, updateDoc, where } from "firebase/firestore";
 
 class FirestoreDAO {
-constructor(nomeColecao) {
-this.colecao = collection(db, nomeColecao);
-}
+    async getAllLocais() {
+        try {
+            const locaisSnapshot = await getDocs(collection(db, 'locais'));
+            const locais = locaisSnapshot.docs.map(doc => {
+                const data = doc.data();
+                return {
+                    id: doc.id,
+                    nome: data.nome,
+                    imagens: data.imagens,
+                    informacoes: data.informacoes,
+                    categoria: data.categoria,
+                    cidade: data.cidade,
+                    mapUrl: data.mapUrl,
+                };
+            });
+        return locais;
+        } catch (error) {
+        console.error('Erro ao obter locais: ', error);
+        throw new Error('Erro ao obter locais');
+        }
+    }
+async getLocalById(id) {
+    try {
+        const localDoc = await getDoc(doc(db, 'locais', id));
+        if (localDoc.exists()) {
+        return { id: localDoc.id, ...localDoc.data() };
+        } else {
+        throw new Error('Local não encontrado');
+        }
+    } catch (error) {
+        console.error('Erro ao obter local:', error);
+        throw new Error('Erro ao obter local');
+    }
+    }
+async getLocaisByCategoria(categoria) {
+    try {
+        const locaisRef = collection(db, 'locais');
+        const q = query(locaisRef, where('categoria', '==', categoria));
+        const locaisSnapshot = await getDocs(q);
 
-async buscarTodos() {
-const snapshot = await getDocs(this.colecao);
-return snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
-}
+        const locais = locaisSnapshot.docs.map(doc => {
+        const data = doc.data();
+        return {
+            id: doc.id,
+            nome: data.nome,
+            imagens: data.imagens,
+            informacoes: data.informacoes,
+            categoria: data.categoria,
+            cidade: data.cidade,
+            mapUrl: data.mapUrl,
+        };
+        });
+        return locais;
+    } catch (error) {
+        console.error('Erro ao obter locais por categoria: ', error);
+        throw new Error('Erro ao obter locais por categoria');
+    }
+    }
+    async addLocal(local) {
+        try {
+        const novoDoc = doc(collection(db, 'locais'));
+        await setDoc(novoDoc, local);
+        console.log('Local adicionado com sucesso!');
+    } catch (error) {
+        console.error('Erro ao adicionar local: ', error);
+        throw new Error('Erro ao adicionar local');
+    }
+    }
 
-
-async buscarPorId(id) {
-const ref = doc(this.colecao, id);
-const snapshot = await getDoc(ref);
-return snapshot.exists() ? { id: snapshot.id, ...snapshot.data() } : null;
+    async updateLocal(id, updates) {
+    try {
+        const localRef = doc(db, 'locais', id);
+        await updateDoc(localRef, updates);
+        console.log('Local atualizado com sucesso!');
+    } catch (error) {
+        console.error('Erro ao atualizar local: ', error);
+        throw new Error('Erro ao atualizar local');
+    }
+    }
 }
-
-async adicionar(dados) {
-const docRef = await addDoc(this.colecao, dados);
-return { id: docRef.id, ...dados };
-}
-
-async atualizar(id, novosDados) {
-const ref = doc(this.colecao, id);
-await updateDoc(ref, novosDados);
-}
-
-async remover(id) {
-const ref = doc(this.colecao, id);
-await deleteDoc(ref);
-}
-}
-
+    
 export default FirestoreDAO;
