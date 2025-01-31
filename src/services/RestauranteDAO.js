dd17161070d05b47a54e4fa340ceafd7fe2bc720
Évelly Silva <silvasevelly@gmail.com>
2025-01-31 18:10:31 -0300

    alteração no DAORestaurante para a que retorne todos os restaurantes com o campo nicho

--- src/services/RestauranteDAO.js
+++ src/services/RestauranteDAO.js
@@ -1,15 +1,61 @@
+import { collection, getDocs, query, where } from "firebase/firestore";
+import { db } from "./firebaseConfig";
 import FirestoreDAO from "./FirestoreDAO";
 
 class RestauranteDAO extends FirestoreDAO {
 constructor() {
-super("restaurantes");
+super();
+this.colecao = "restaurantes";
 }
 
+async getAllRestaurantes() {
+    try {
+    const restaurantesSnapshot = await getDocs(collection(db, this.colecao));
+
+    const restaurantes = restaurantesSnapshot.docs.map(doc => {
+    const data = doc.data();
+    return {
+        id: doc.id,
+        nome: data.nome,
+        imagens: data.imagens || [],
+        informacoes: data.informacoes,
+        categoria: data.categoria,
+        cidade: data.cidade,
+        mapUrl: data.mapUrl,
+        nicho: data.nicho
+    };
+    });
+    return restaurantes;
+} catch (error) {
+    console.error("Erro ao obter restaurantes: ", error);
+    throw new Error("Erro ao obter restaurantes");
+}
+}
 async buscarPorNicho(nicho) {
-const restaurantes = await this.buscarTodos();
-return restaurantes.filter(restaurante => restaurante.nicho === nicho);
+try {
+    const restaurantesRef = collection(db, this.colecao);
+    const q = query(restaurantesRef, where("nicho", "==", nicho));
+    const restaurantesSnapshot = await getDocs(q);
+
+    const restaurantes = restaurantesSnapshot.docs.map(doc => {
+        const data = doc.data();
+        return {
+        id: doc.id,
+        nome: data.nome,
+        imagens: data.imagens || [],
+        informacoes: data.informacoes,
+        categoria: data.categoria,
+        cidade: data.cidade,
+        mapUrl: data.mapUrl,
+        nicho: data.nicho
+        };
+    });
+    return restaurantes;
+    } catch (error) {
+    console.error("Erro ao obter restaurantes por nicho: ", error);
+    throw new Error("Erro ao obter restaurantes por nicho");
+    }
 }
 }
 
 export default RestauranteDAO;
-
