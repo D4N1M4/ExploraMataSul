2dbe58fcd08650fd392ffa523efa729fda42c50c
Daniele <danielemaria1407@gmail.com>
2025-02-16 18:24:37 -0300

    Corrigindo exibição de favoritos

--- src/components/LocaisFavoritos.vue
+++ src/components/LocaisFavoritos.vue
@@ -30,8 +30,12 @@ import { onMounted, ref } from 'vue';
 import { auth, db } from '../firebase';
 import Favoritar from './Favoritar.vue';
 import RestauranteCard from './RestauranteCard.vue';
+import FirestoreDAO from '@/services/FirestoreDAO';
+import { FirebaseError } from 'firebase/app';
 
 const favoritos = ref([]);
+
+const restauranteDAO = new FirestoreDAO();
 
 const carregarFavoritos = async () => {
 const user = auth.currentUser;
@@ -40,7 +44,15 @@ if (!user) return;
 const favoritosRef = collection(db, `usuarios/${user.uid}/favoritos`);
 const snapshot = await getDocs(favoritosRef);
 
-favoritos.value = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+const resultFavorit = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data()}));
+const listFav = [];
+for (let index = 0; index < resultFavorit.length; index++){
+    const element = resultFavorit[index];
+
+    const result = await restauranteDAO.getById(element.id);
+    listFav.push(result)
+}
+favoritos.value = listFav;
 
 console.log("Locais favoritos carregados:", favoritos.value);
 };
